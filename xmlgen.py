5c411772a22b04260171886bf27321002680da53
ArthurDayot <ArthurDayot@users.noreply.github.com>
2019-12-18 14:46:58 -0800

    Modified memcache lines

--- xmlgen.py
+++ xmlgen.py
@@ -73,41 +73,58 @@ def get_xml():
     #email = query_parameters.get('email')
     #playlistname = query_parameters.get('playlistname')
      
+    client = base.Client(('localhost', 11211)) #Declare the memcache object here
+    # Key is string email concat to string playlistname
     payload = {'email':'e@gmail.com','playlistname':'tunes'}
+    outputString = ""
     
-    outputString = "Playlist id: " + "\r\r\n"
-    
-    #GET list of song's uuid
-    songsInPlaylist = requests.get('http://localhost:8000/playlist/playlist/select/tracks', params=payload)
-    songsInPlaylistJSON = songsInPlaylist.json() #list of songs
-    
-    #get track info: title, artist, album
-    #get track data from uuid  
-    #        outputString += "Track: " + str(track) + "\r\r\n"
-    outputStringXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\r\n<playlist version=\"1\" xmlns=\"http://xspf.org/ns/0/\">\r\r\n\t<title>"+ "playlist" + "</title>\r\r\n\t<trackList>\r\r\n"
+    key = payload['email'] + payload['playlistname']
 
-    for track in songsInPlaylistJSON:
-        payload = {'id' : track }
-        trackData = requests.get('http://localhost:8000/tracks/get/track/id', params=payload)
-        trackDataJSON = trackData.json()
-        outputString += "Title: " + str(trackDataJSON[0]['title']) + "\r\r\n"
-        outputString += "Artist: " + str(trackDataJSON[0]['artist']) + "\r\r\n"
-        outputString += "Album: " + str(trackDataJSON[0]['album']) + "\r\r\n"
-        outputString += "URL: " + str(trackDataJSON[0]['url']) + "\r\r\n"
-        outputStringXML += "\t\t<track>\r\r\n"
-        outputStringXML += "\t\t\t<location> " + str(trackDataJSON[0]['url']) + "</location>\r\r\n"
-        outputStringXML += "\t\t\t<title>" + str(trackDataJSON[0]['title']) + "</title>\r\r\n"
-        outputStringXML += "\t\t\t<creator>" + str(trackDataJSON[0]['artist']) + "</creator>\r\r\n"
-        outputStringXML += "\t\t\t<album> " + str(trackDataJSON[0]['album']) + "</album>\r\r\n"
-        outputStringXML += "\t\t</track>\r\r\n"
+    #Check cache for data
+    memcacheresult = client.get(key)
+
+    if memcacheresult is None: #If data is not found, generate xml string, and store into cache
+
+        outputString += "Data not found! Calling microservices.\r\r\nPlaylist id: " + "\r\r\n"
+
+        #GET list of song's uuid
+        songsInPlaylist = requests.get('http://localhost:5000/playlist/playlist/select/tracks', params=payload)
+        songsInPlaylistJSON = songsInPlaylist.json() #list of songs
+    
+        #get track info: title, artist, album
+        #get track data from uuid  
+        #        outputString += "Track: " + str(track) + "\r\r\n"
+        outputStringXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\r\n<playlist version=\"1\" xmlns=\"http://xspf.org/ns/0/\">\r\r\n\t<title>"+ "playlist" + "</title>\r\r\n\t<trackList>\r\r\n"
+
+        for track in songsInPlaylistJSON:
+            payload = {'id' : track }
+            trackData = requests.get('http://localhost:5100/tracks/get/track/id', params=payload)
+            trackDataJSON = trackData.json()
+            outputString += "Title: " + str(trackDataJSON[0]['title']) + "\r\r\n"
+            outputString += "Artist: " + str(trackDataJSON[0]['artist']) + "\r\r\n"
+            outputString += "Album: " + str(trackDataJSON[0]['album']) + "\r\r\n"
+            outputString += "URL: " + str(trackDataJSON[0]['url']) + "\r\r\n"
+            outputStringXML += "\t\t<track>\r\r\n"
+            outputStringXML += "\t\t\t<location> " + str(trackDataJSON[0]['url']) + "</location>\r\r\n"
+            outputStringXML += "\t\t\t<title>" + str(trackDataJSON[0]['title']) + "</title>\r\r\n"
+            outputStringXML += "\t\t\t<creator>" + str(trackDataJSON[0]['artist']) + "</creator>\r\r\n"
+            outputStringXML += "\t\t\t<album> " + str(trackDataJSON[0]['album']) + "</album>\r\r\n"
+            outputStringXML += "\t\t</track>\r\r\n"
         
-    outputStringXML += "\t</trackList>\r\r\n</playlist>"
+        outputStringXML += "\t</trackList>\r\r\n</playlist>"
+
+        #client.set(key,outputStringXML)
+        #set(key, value, expire=0, noreply=None, flags=None)
+        client.set(key,outputStringXML,9001)
+        #client.set(key,outputStringXML,9001,true,0)
+
+    else:
+        outputStringXML = memcacheresult
+        outputString += "Data in cache found!\r\r\n"
+
     from xml.etree import ElementTree
 
     root = ElementTree.fromstring(outputStringXML)
     myfile = open("test.xml","w+")
     myfile.write(outputStringXML)
     return outputString
-
-    
-
